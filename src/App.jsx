c695d6fde572aa1c676c8db7f2bdfac4efdfaa2c
soft1986 <softtec1986@gmail.com>
2025-12-31 16:10:01 +0500

    name changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/navbar';
 import Home from './Pages/home';
-
+import './App.css'
 const App = () => {
   return (
     <BrowserRouter basename="/Portofolio">
